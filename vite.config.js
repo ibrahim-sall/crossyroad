edbc6c245cf3c2d4db5e83d2f088eb137a1ae40d
ibrahim-sall <ibhou2002@gmail.com>
2025-02-19 19:09:30 +0100

    try to patch workflow

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { viteStaticCopy } from 'vite-plugin-static-copy';
 import glsl from 'vite-plugin-glsl';
 
 export default defineConfig({
-    base: "/three_vite/",
+    base: "/three_vite/ibrahim-sall",
     clearScreen: false,
     optimizeDeps: {
         esbuildOptions: {
