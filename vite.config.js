1ee36a8797e075edcb92051636100759cecc8a36
ibrahim-sall <76476132+ibrahim-sall@users.noreply.github.com>
2025-02-20 10:50:01 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { viteStaticCopy } from 'vite-plugin-static-copy';
 import glsl from 'vite-plugin-glsl';
 
 export default defineConfig({
-    base: "/",
+    base: "/crossyroad",
     clearScreen: false,
     optimizeDeps: {
         esbuildOptions: {
